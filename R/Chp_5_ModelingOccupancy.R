84fc488c6de50047660b6f7c95ae750115a3d65f
Dave Eslinger <dave.eslinger01@gmail.com>
2025-11-07 19:03:02 -0700

    First examples

---
+++ R/Chp_5_ModelingOccupancy.R
@@ -0,0 +1,59 @@
+# Chapter 5 modweling occupancy
+
+# Sec 5.1 ----
+library(auk)
+library(lubridate)
+library(sf)
+library(dggridR)
+library(unmarked)
+library(raster)
+library(ebirdst)
+library(MuMIn)
+library(AICcmodavg)
+library(fields)
+library(tidyverse)
+# resolve namespace conflicts
+select <- dplyr::select
+projection <- raster::projection
+
+# set random number seed to insure fully repeatable results
+set.seed(1)
+
+# setup output directory for saved results
+if (!dir.exists("output")) {
+  dir.create("output")
+}
+
+# ebird data
+ebird <- read_csv("data/ebd_woothr_june_bcr27_zf.csv") %>% 
+  mutate(year = year(observation_date),
+         # occupancy modeling requires an integer response
+         species_observed = as.integer(species_observed))
+
+# modis land cover covariates
+habitat <- read_csv("data/pland-elev_location-year.csv") %>% 
+  mutate(year = as.integer(year))
+
+# combine ebird and modis data
+ebird_habitat <- inner_join(ebird, habitat, by = c("locality_id", "year"))
+
+# prediction surface
+pred_surface <- read_csv("data/pland-elev_prediction-surface.csv")
+# latest year of landcover data
+max_lc_year <- pred_surface$year[1]
+r <- raster("data/prediction-surface.tif")
+
+# load gis data for making maps
+map_proj <- st_crs(102003)
+ne_land <- read_sf("data/gis-data.gpkg", "ne_land") %>% 
+  st_transform(crs = map_proj) %>% 
+  st_geometry()
+bcr <- read_sf("data/gis-data.gpkg", "bcr") %>% 
+  st_transform(crs = map_proj) %>% 
+  st_geometry()
+ne_country_lines <- read_sf("data/gis-data.gpkg", "ne_country_lines") %>% 
+  st_transform(crs = map_proj) %>% 
+  st_geometry()
+ne_state_lines <- read_sf("data/gis-data.gpkg", "ne_state_lines") %>% 
+  st_transform(crs = map_proj) %>% 
+  st_geometry(
